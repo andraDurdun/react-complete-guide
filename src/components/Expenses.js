be77a3d5cf23d266fe1361758377d3552fbf128f
Andra Durdun <andra.durdun@gmail.com>
2022-05-30 18:24:28 +0300

    Composition

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount}
                          date={props.items[0].date}/>
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount}
@@ -12,7 +13,7 @@ function Expenses(props) {
                          date={props.items[2].date}/>
             <ExpenseItem title={props.items[3].title} amount={props.items[3].amount}
                          date={props.items[3].date}/>
-        </div>
+        </Card>
     )
 }
 
