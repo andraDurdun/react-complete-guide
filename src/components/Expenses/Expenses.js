cf2e197da1c98944208c94ed15751b85e4fcce1b
Andra Durdun <andra.durdun@gmail.com>
2022-06-09 17:38:08 +0300

    Using stateful lists

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,6 +7,7 @@ import {useState} from "react";
 const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState("2020");
+
     const filterChangeHandler = (selectedYear) => {
         console.log(selectedYear);
         setFilteredYear(selectedYear);
@@ -16,14 +17,13 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount}
-                             date={props.items[0].date}/>
-                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount}
-                             date={props.items[1].date}/>
-                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount}
-                             date={props.items[2].date}/>
-                <ExpenseItem title={props.items[3].title} amount={props.items[3].amount}
-                             date={props.items[3].date}/>
+                {
+                    props.items.map(
+                        expense =>
+                            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount}
+                                         date={expense.date}/>
+                    )
+                }
             </Card>
         </div>
     )
