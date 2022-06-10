be75b7cceb5d39cb45cbd050f66d57a26684b26f
Andra Durdun <andra.durdun@gmail.com>
2022-06-10 16:19:35 +0300

    Filter by year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,16 +9,17 @@ const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState("2020");
 
     const filterChangeHandler = (selectedYear) => {
-        console.log(selectedYear);
         setFilteredYear(selectedYear);
     }
+
+    const filteredExpenses = props.items.filter(item => item.date.getFullYear().toString() === filteredYear);
 
     return (
         <div>
             <Card className="expenses">
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
                 {
-                    props.items.map(
+                    filteredExpenses.map(
                         expense =>
                             <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount}
                                          date={expense.date}/>
